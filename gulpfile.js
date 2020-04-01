1ae7e704072130c107e3523364fb1f1dfb2bd513
Matthew Bloomfield <mbloomfield@uschamber.com>
2020-04-01 14:29:02 -0400

    Mostly there with widths, some padding issues, no mobile checks

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,63 @@
-const { src, dest, series } = require('gulp');
+const { src, dest, watch, series, parallel } = require('gulp');
 const fileinclude = require('gulp-file-include');
 const concat = require('gulp-concat');
 const clean = require('gulp-clean');
+const gls = require('gulp-live-server');
+
+const files = {
+  html: {
+    dev: ['./src/index.html'],
+    components: ['./src/components/*.html']
+  },
+  css: {
+    dev: ['./src/css/base.css', './src/css/custom.css', './src/components/*.css'],
+    tmp: {
+      name: 'style.css',
+      path: './.tmp'
+    }  
+  },
+  dest: {
+    path: './dist'
+  }
+}
 
 function includeHtmlComponents() {
-  return src(['src/index.html'])
+  return src(files.html.dev)
     .pipe(fileinclude({
       prefix: '@@',
       basepath: '@file'
     }))
-    .pipe(dest('./dist/'));
+    .pipe(dest(files.dest.path));
 }
 
 function concatCss() {
-  return src(['src/css/base.css', 'src/css/custom.css', 'src/css/components/*'])
-    .pipe(concat('style.css'))
-    .pipe(dest('./tmp/'));
+  return src(files.css.dev)
+    .pipe(concat(files.css.tmp.name))
+    .pipe(dest(files.css.tmp.path));
 }
 
 function removeTmpFiles() {
-  return src('./tmp', { read: false })
+  return src([files.css.tmp.path], { read: false })
     .pipe(clean());
 }
 
-exports.default = series(concatCss, includeHtmlComponents, removeTmpFiles);
+function startServer() {
+  const server = gls.static(files.dest.path, 3256);
+  server.start();
+  console.log('watching the following files', [...files.html.dev, ...files.css.dev]);
+  watch([...files.html.dev, ...files.css.dev, ...files.html.components], series(concatCss, includeHtmlComponents, removeTmpFiles));
+}
+
+function build() {
+  console.log('building fresh copy...');
+  return series(concatCss, parallel(includeHtmlComponents, copyAssets), removeTmpFiles);
+}
+
+function copyAssets() {
+  return src('./src/img/*')
+    .pipe(dest('./dist/'));
+}
+
+exports.default = build();
+exports.dev = series(build(), startServer);
+exports.test = series(concatCss, includeHtmlComponents, removeTmpFiles);
